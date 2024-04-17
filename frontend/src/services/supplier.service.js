29ded8de5a864ad0b5a80b85247ba945934416de
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-04-16 18:26:44 -0600

    Update supplier.service.js

--- frontend/src/services/supplier.service.js
+++ frontend/src/services/supplier.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL_API = "http://localhost:3000/api/suppliers";
+const URL_API = "https://sitioweb-z4g2.onrender.com/api/suppliers";
 
 export function getSupplierInfo() {
   return axios.get(`${URL_API}`, { withCredentials: true });
