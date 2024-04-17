3a7c9072a786ea8c15c944ef23d9b338a8c49287
JesusMorales19 <138167542+JesusMorales19@users.noreply.github.com>
2024-04-16 23:48:04 -0600

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb://localhost:27017/login";
+const MONGO_URL = "mongodb+srv://fernanda:1234@cluster0.6j8qpyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 export const connectDB = async () => {
   try {
